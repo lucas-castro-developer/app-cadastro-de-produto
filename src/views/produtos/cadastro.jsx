595eb78163bcbbc5516d9c2635750d235ad29a3c
Lucas Castro <lucas.sc98@hotmail.com>
2021-06-05 13:33:21 -0300

    Creating add product card

---
+++ src/views/produtos/cadastro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class CadastroProduto extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">Cadastro de produto</div>
+        <div className="card-body"></div>
+      </div>
+    );
+  }
+}
+
+export default CadastroProduto;
