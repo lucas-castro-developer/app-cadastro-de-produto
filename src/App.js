bd5ef4be3d4d9c89c6338f8684d3110d138d3bb9
Lucas Castro <lucas.sc98@hotmail.com>
2021-06-05 00:14:40 -0300

    Installing bootswatch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="alert alert-dismissible alert-warning">
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+        ></button>
+        <h4 className="alert-heading">Warning!</h4>
+        <p className="mb-0">
+          Hello
+          <a href="#" className="alert-link">
+            Bootswatch
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import 'bootswatch/dist/cerulean/bootstrap.min.css'
 import "./index.css";
 import App from "./App";
 
