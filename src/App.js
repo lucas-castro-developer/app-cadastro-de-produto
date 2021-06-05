4e37a83d45534848b132d65232c6867ba6320321
Lucas Castro <lucas.sc98@hotmail.com>
2021-06-05 00:31:01 -0300

    Creating Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
 
+import Navbar from './components/navbar'
+
 function App() {
   return (
-    <div className="App">
-      <div className="alert alert-dismissible alert-warning">
-        <button
-          type="button"
-          className="btn-close"
-          data-bs-dismiss="alert"
-        ></button>
-        <h4 className="alert-heading">Warning!</h4>
-        <p className="mb-0">
-          Hello
-          <a href="#" className="alert-link">
-            Bootswatch
-          </a>
-          .
-        </p>
-      </div>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
